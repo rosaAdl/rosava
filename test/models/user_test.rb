f44ec8ad65c63ae287e600f1afd92b4e7af726a6
Rosa Adl <rossa.adl@gmail.com>
2023-08-07 08:22:56 -0600

    Adding first user test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(email: "new@test.com", password: "12345678", password_confirmation: "12345678")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
 end

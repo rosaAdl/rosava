656613a38a1057f637b2d36b352d29f3e4b250a3
Rosa Adl <rossa.adl@gmail.com>
2023-08-07 19:27:41 -0600

    "lets not send email"

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,8 +64,10 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.raise_delivery_errors = false
+
   # Devise
-  config.action_mailer.default_url_options = { host: 'rosava.onrender.com' }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
